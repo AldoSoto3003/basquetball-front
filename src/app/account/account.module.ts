45abea2b4f6f37c86b42bd6759cabe2fac3185ec
dimasmnat <114051480+dimasmnat@users.noreply.github.com>
2023-05-03 20:15:40 -0700

    corrección
    errores  del merge corregidos

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -4,14 +4,13 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RecoveryPasswordComponent } from './recovery-password/recovery-password.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ConfigComponent } from './config/config.component';
+
 
 @NgModule({
   declarations: [
     LoginComponent,
     RegisterComponent,
     RecoveryPasswordComponent,
-    ConfigComponent,
   ],
   imports: [
     CommonModule,
