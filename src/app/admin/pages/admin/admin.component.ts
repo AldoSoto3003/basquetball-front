5f0e2c4d670f3778a175e3fa50f849acc203b086
Aldo Soto <aldosoto3003@gmail.com>
2023-05-07 18:38:36 -0700

    avance ngifs

--- src/app/admin/pages/admin/admin.component.ts
+++ src/app/admin/pages/admin/admin.component.ts
@@ -1,3 +1,4 @@
+import { Breakpoints } from '@angular/cdk/layout';
 import { AfterViewInit, Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Usuario } from 'src/app/models/Usuario.model';
@@ -12,21 +13,47 @@ export class AdminComponent implements AfterViewInit  {
   
   collapsed = true;
   datosUsuario:Usuario;
-  
+
+  admonGeneral: boolean = false;
+  admonTorneo: boolean = false;
+  Jugador: boolean = false;
+  entrenador: boolean = false;
+  arbitro: boolean = false;
+
   constructor(private router:Router, private authService:AuthService){
-    this.ObtenerDatos(); 
+    // this.ObtenerDatos(); 
   }
 
   ngAfterViewInit(): void {
       this.ObtenerDatos(); 
+
   }
 
 
   ObtenerDatos() {
     this.authService.authme().subscribe(x => {
       localStorage.setItem("informacion_usuario", JSON.stringify(x.data));
+      console.log(x.data.Id_Rol)
+      switch(x.data.Id_Rol){
+        case 1:
+          this.admonGeneral = true;
+          break;
+        case 2:
+          this.admonTorneo = true;
+          break;
+        case 3:
+          this.Jugador = true;
+          break;
+        case 4:
+          this.entrenador = true;
+          break;
+        case 5:
+          this.arbitro = true;
+          break;
+        default:
+          break;
+      } 
     },)
-
     this.datosUsuario = JSON.parse(localStorage.getItem("informacion_usuario")!);
   }
 
